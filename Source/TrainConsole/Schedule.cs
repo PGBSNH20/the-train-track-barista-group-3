865c039232acbd0a9ab3c4928fdb7c8548bd6930
ItsAnass <domanee@hotmail.com>
2021-03-10 16:33:12 +0100

    Added som prop to some classes.

--- Source/TrainConsole/Schedule.cs
+++ Source/TrainConsole/Schedule.cs
@@ -10,30 +10,35 @@ namespace TrainConsole
     public class Schedule
     {
         public int TrainId { get; set; }
-        public int StationId { get; set; }
-        public string DepartureTime { get; set; } 
+        public int DepStationId { get; set; }
+        public string DepartureTime { get; set; }
+        public int ArrvStationId { get; set; }
         public string ArrivalTime { get; set; }
+        
 
 
         const string FilePath = @"C:\Users\doman\OneDrive\Desktop\RailWayProject\Source\TrainConsole\Data\timetable.txt";
 
-        public Schedule(int trainid,int stationid,string departuretime,string arrivaltime)
+        public Schedule(int trainid,int depstationid,string departuretime, int arrStationId,string arrivaltime)
         {
             TrainId = trainid;
-            StationId = stationid;
+            DepStationId = depstationid;           
             DepartureTime = departuretime;
+            ArrvStationId = arrStationId;
             ArrivalTime = arrivaltime;
+            
         }
 
         public static Schedule CreateFromLine(string line)
         {
             string[] parts = line.Split(',');
-            Schedule p = new Schedule(int.Parse(parts[0]), int.Parse(parts[1]), parts[2], parts[3])
+            Schedule p = new Schedule(int.Parse(parts[0]), int.Parse(parts[1]), parts[2], int.Parse(parts[3]), parts[4])
             {
                 TrainId = int.Parse(parts[0]),
-                StationId = int.Parse(parts[1]),
+                DepStationId = int.Parse(parts[1]),
                 DepartureTime = parts[2],
-                ArrivalTime = parts[3]
+                ArrvStationId= int.Parse(parts[3]),
+                ArrivalTime = parts[4]
             };
             return p;
         }
