f60aa5d1e18b279005f59a662883f09278339346
ItsAnass <domanee@hotmail.com>
2021-03-10 11:51:21 +0100

    created an interface and a class with methods where we can add and remove passengers and get the amount of total passengers

---
+++ Source/TrainEngine/IPassengerCart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainEngine
+{
+    interface IPassengerCart
+    {
+
+        void AddPassengers(int amount);
+        void RemovePassengers(int amount);
+        int GetPassengerAmount();
+
+
+    }
+}
---
+++ Source/TrainEngine/PassengerCart.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainEngine
+{
+    class PassengerCart : IPassengerCart
+    {
+        private int passengerAmount;
+
+
+        public PassengerCart(int passengers=0)
+        {
+            if (passengers<0)
+            {
+                passengerAmount = 0;
+            }
+            else
+            {
+                passengerAmount = passengers;
+            }
+        }
+
+
+
+        public void AddPassengers(int amount)
+        {
+            passengerAmount += amount;
+        }
+
+        
+
+        public void RemovePassengers(int amount)
+        {
+            if (passengerAmount-amount<0)
+            {
+                passengerAmount = 0;
+            }
+            else
+            {
+                passengerAmount -= amount;
+            }
+        }
+
+        public int GetPassengerAmount()
+        {
+            return passengerAmount;
+        }
+    }
+}
